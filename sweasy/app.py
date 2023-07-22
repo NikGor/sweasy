d34b4e976fa1452f3d45b2a3dd1eeb0c91a2e139
Nikolai <nicolas.gordienko@gmail.com>
2023-07-23 00:04:38 +0200

    chore: Create initial version of the main page

--- sweasy/app.py
+++ sweasy/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sweasy.db'
+db = SQLAlchemy(app)
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cards = Card.query.all()
+    return render_template('index.html', cards=cards)
 
 
 if __name__ == '__main__':
